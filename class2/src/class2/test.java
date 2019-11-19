4fa1bd4c546a601b34ca07d9318abf5262123450
jun hyuk Im <amigojun@naver.com>
2019-11-20 01:00:14 +0900

    added print line

--- class2/src/class2/test.java
+++ class2/src/class2/test.java
@@ -3,5 +3,10 @@ package class2;
 public class test {
  public static void main(String[]args) {
 	 System.out.println("Wonkwang University Computer S/W Department");
+	 System.out.println("1st Grade");
+	 System.out.println("2nd Grade");
+	 System.out.println("3rd Grade");
+	 System.out.println("4th Grade");
+	 
  }
 }
